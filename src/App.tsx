183f6ba4e5abc6912e849fb130614e0c017313e7
tsucchiiinoko <ccya2211@icloud.com>
2022-07-15 18:29:22 +0900

    tauri appを作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react"; // 追加
+import { invoke } from "@tauri-apps/api"; // 追加
 
 function App() {
+  const [message, setMessage] = useState("");
+
+  const messageHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setMessage(e.target.value);
+    console.log(message);
+  };
+
+  const GreetHandler = async (name: string) => {
+    const response = await invoke("greet", { name: name });
+    alert(response);
+    setMessage("");
+  };
+
   return (
     <div className="App">
       <header className="App-header">
@@ -16,8 +31,12 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React & Tauri !!
         </a>
+        {/* 追加 */}
+        <p>メッセージ: {message}</p>
+        <input onChange={(e) => messageHandler(e)}></input>
+        <button onClick={() => GreetHandler(message)}>ようこそ</button>
       </header>
     </div>
   );
